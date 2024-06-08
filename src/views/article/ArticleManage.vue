168b9caf2804382bdd27c11c106068a20e610047
wearetogetherforever <3156552287@qq.com>
2024-06-08 08:40:45 +0800

    修改4

--- src/views/article/ArticleManage.vue
+++ src/views/article/ArticleManage.vue
@@ -5,74 +5,79 @@ import ChannelSelect from './components/ChannelSelect.vue'
 import ArticleEdit from './components/ArticleEdit.vue'
 import { artGetListService, artDelService } from '@/api/article.js'
 import { formatTime } from '@/utils/format.js'
-const articleList = ref([]) // 文章列表
-const total = ref(0) // 总条数
-const loading = ref(false) // loading状态
 
-// 定义请求参数对象
+// 初始化文章列表和总条数
+const articleList = ref([])
+const total = ref(0)
+// 控制加载状态
+const loading = ref(false)
+
+// 初始化请求参数
 const params = ref({
-  pagenum: 1, // 当前页
-  pagesize: 5, // 当前生效的每页条数
+  pagenum: 1,
+  pagesize: 5,
   cate_id: '',
   state: ''
 })
 
-// 基于params参数，获取文章列表
+/**
+ * 获取文章列表
+ */
 const getArticleList = async () => {
   loading.value = true
-
   const res = await artGetListService(params.value)
   articleList.value = res.data.data
   total.value = res.data.total
-
   loading.value = false
 }
+
+// 初始化时调用获取列表
 getArticleList()
 
-// 处理分页逻辑
+//#region 分页相关
+// 处理分页大小变化
 const onSizeChange = (size) => {
-  // console.log('当前每页条数', size)
-  // 只要是每页条数变化了，那么原本正在访问的当前页意义不大了，数据大概率已经不在原来那一页了
-  // 重新从第一页渲染即可
   params.value.pagenum = 1
   params.value.pagesize = size
-  // 基于最新的当前页 和 每页条数，渲染数据
   getArticleList()
 }
+
+// 处理页码变化
 const onCurrentChange = (page) => {
-  // 更新当前页
   params.value.pagenum = page
-  // 基于最新的当前页，渲染数据
   getArticleList()
 }
+//#endregion
 
-// 搜索逻辑 => 按照最新的条件，重新检索，从第一页开始展示
+// 搜索操作
 const onSearch = () => {
-  params.value.pagenum = 1 // 重置页面
+  params.value.pagenum = 1
   getArticleList()
 }
 
-// 重置逻辑 => 将筛选条件清空，重新检索，从第一页开始展示
+// 重置操作
 const onReset = () => {
-  params.value.pagenum = 1 // 重置页面
+  params.value.pagenum = 1
   params.value.cate_id = ''
   params.value.state = ''
   getArticleList()
 }
 
+// 文章编辑引用
 const articleEditRef = ref()
-// 添加逻辑
+
+// 打开添加文章操作
 const onAddArticle = () => {
   articleEditRef.value.open({})
 }
-// 编辑逻辑
+
+// 打开编辑文章操作
 const onEditArticle = (row) => {
   articleEditRef.value.open(row)
 }
 
-// 删除逻辑
+// 删除文章操作
 const onDeleteArticle = async (row) => {
-  // 提示用户是否要删除
   await ElMessageBox.confirm('此操作将永久删除该文件, 是否继续?', '提示', {
     confirmButtonText: '确定',
     cancelButtonText: '取消',
@@ -80,19 +85,15 @@ const onDeleteArticle = async (row) => {
   })
   await artDelService(row.id)
   ElMessage.success('删除成功')
-  // 重新渲染列表
   getArticleList()
 }
 
-// 添加或者编辑 成功的回调
+// 文章添加或编辑成功回调
 const onSuccess = (type) => {
   if (type === 'add') {
-    // 如果是添加，最好渲染最后一页
     const lastPage = Math.ceil((total.value + 1) / params.value.pagesize)
-    // 更新成最大页码数，再渲染
     params.value.pagenum = lastPage
   }
-
   getArticleList()
 }
 </script>
@@ -103,18 +104,11 @@ const onSuccess = (type) => {
       <el-button type="primary" @click="onAddArticle">添加文章</el-button>
     </template>
 
-    <!-- 表单区域 -->
     <el-form inline>
       <el-form-item label="文章分类:">
-        <!-- Vue2 => v-model :value 和 @input 的简写 -->
-        <!-- Vue3 => v-model :modelValue 和 @update:modelValue 的简写 -->
         <channel-select v-model="params.cate_id"></channel-select>
-
-        <!-- Vue3 => v-model:cid  :cid 和 @update:cid 的简写 -->
-        <!-- <channel-select v-model:cid="params.cate_id"></channel-select> -->
       </el-form-item>
       <el-form-item label="发布状态:">
-        <!-- 这里后台标记发布状态，就是通过中文标记的，已发布 / 草稿 -->
         <el-select v-model="params.state">
           <el-option label="已发布" value="已发布"></el-option>
           <el-option label="草稿" value="草稿"></el-option>
@@ -126,7 +120,6 @@ const onSuccess = (type) => {
       </el-form-item>
     </el-form>
 
-    <!-- 表格区域 -->
     <el-table :data="articleList" v-loading="loading">
       <el-table-column label="文章标题" prop="title">
         <template #default="{ row }">
@@ -140,7 +133,6 @@ const onSuccess = (type) => {
         </template>
       </el-table-column>
       <el-table-column label="状态" prop="state"></el-table-column>
-      <!-- 利用作用域插槽 row 可以获取当前行的数据 => v-for 遍历 item -->
       <el-table-column label="操作">
         <template #default="{ row }">
           <el-button
@@ -161,7 +153,6 @@ const onSuccess = (type) => {
       </el-table-column>
     </el-table>
 
-    <!-- 分页区域 -->
     <el-pagination
       v-model:current-page="params.pagenum"
       v-model:page-size="params.pagesize"
@@ -174,7 +165,6 @@ const onSuccess = (type) => {
       style="margin-top: 20px; justify-content: flex-end"
     />
 
-    <!-- 添加编辑的抽屉 -->
     <article-edit ref="articleEditRef" @success="onSuccess"></article-edit>
   </page-container>
 </template>
--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -11,105 +11,87 @@ import {
 } from '@/api/article'
 import { baseURL } from '@/utils/request'
 import axios from 'axios'
-// 控制抽屉显示隐藏
+
+// 控制抽屉的显示和隐藏
 const visibleDrawer = ref(false)
 
-// 默认数据
+// 定义默认表单数据结构
 const defaultForm = {
   title: '', // 标题
-  cate_id: '', // 分类id
-  cover_img: '', // 封面图片 file 对象
-  content: '', // string 内容
-  state: '' // 状态
+  cate_id: '', // 分类ID
+  cover_img: '', // 封面图片
+  content: '', // 文章内容
+  state: '' // 文章状态
 }
 
-// 准备数据
+// 初始化表单模型
 const formModel = ref({ ...defaultForm })
 
-// 图片上传相关逻辑
+// 用于存储上传的图片URL
 const imgUrl = ref('')
+
+// 处理文件选择事件，用于预览和设置封面图片
 const onSelectFile = (uploadFile) => {
-  imgUrl.value = URL.createObjectURL(uploadFile.raw) // 预览图片
-  // 立刻将图片对象，存入 formModel.value.cover_img 将来用于提交
+  imgUrl.value = URL.createObjectURL(uploadFile.raw)
   formModel.value.cover_img = uploadFile.raw
 }
 
-// 提交
+// 定义发表或保存文章的逻辑
 const emit = defineEmits(['success'])
 const onPublish = async (state) => {
-  // 将已发布还是草稿状态，存入 formModel
   formModel.value.state = state
 
-  // 注意：当前接口，需要的是 formData 对象
-  // 将普通对象 => 转换成 => formData对象
+  // 将表单数据转换为FormData对象
   const fd = new FormData()
   for (let key in formModel.value) {
     fd.append(key, formModel.value[key])
   }
 
-  // 发请求
+  // 根据文章是否有ID来判断是发布新文章还是编辑已有的文章
   if (formModel.value.id) {
-    // 编辑操作
     await artEditService(fd)
     ElMessage.success('修改成功')
     visibleDrawer.value = false
     emit('success', 'edit')
   } else {
-    // 添加操作
     await artPublishService(fd)
     ElMessage.success('添加成功')
     visibleDrawer.value = false
-    // 通知到父组件，添加成功了
     emit('success', 'add')
   }
 }
 
-// 组件对外暴露一个方法 open，基于open传来的参数，区分添加还是编辑
-// open({})  => 表单无需渲染，说明是添加
-// open({ id, ..., ... })  => 表单需要渲染，说明是编辑
-// open调用后，可以打开抽屉
+// 打开编辑或添加文章的抽屉，并根据传入的ID加载文章详情
 const editorRef = ref()
 const open = async (row) => {
-  visibleDrawer.value = true // 显示抽屉
+  visibleDrawer.value = true
 
   if (row.id) {
-    // 需要基于 row.id 发送请求，获取编辑对应的详情数据，进行回显
     const res = await artGetDetailService(row.id)
     formModel.value = res.data.data
-    // 图片需要单独处理回显
     imgUrl.value = baseURL + formModel.value.cover_img
-    // 注意：提交给后台，需要的数据格式，是file对象格式
-    // 需要将网络图片地址 => 转换成 file对象，存储起来, 将来便于提交
     const file = await imageUrlToFileObject(
       imgUrl.value,
       formModel.value.cover_img
     )
     formModel.value.cover_img = file
   } else {
-    formModel.value = { ...defaultForm } // 基于默认的数据，重置form数据
-    // 这里重置了表单的数据，但是图片上传img地址，富文本编辑器内容 => 需要手动重置
+    formModel.value = { ...defaultForm }
     imgUrl.value = ''
     editorRef.value.setHTML('')
   }
 }
 
-// 将网络图片地址转换为 File 对象的函数
+// 将图片URL转换为File对象
 async function imageUrlToFileObject(imageUrl, filename) {
   try {
-    // 使用 Axios 下载图片数据
     const response = await axios.get(imageUrl, { responseType: 'arraybuffer' })
-
-    // 将下载的数据转换成 Blob 对象
     const blob = new Blob([response.data], {
       type: response.headers['content-type']
     })
-
-    // 创建 File 对象
-    const file = new File([blob], filename, {
+    return new File([blob], filename, {
       type: response.headers['content-type']
     })
-
-    return file
   } catch (error) {
     console.error('Error converting image URL to File object:', error)
     return null
@@ -128,7 +110,6 @@ defineExpose({
     direction="rtl"
     size="50%"
   >
-    <!-- 发表文章表单 -->
     <el-form :model="formModel" ref="formRef" label-width="100px">
       <el-form-item label="文章标题" prop="title">
         <el-input v-model="formModel.title" placeholder="请输入标题"></el-input>
@@ -140,10 +121,6 @@ defineExpose({
         ></channel-select>
       </el-form-item>
       <el-form-item label="文章封面" prop="cover_img">
-        <!-- 此处需要关闭 element-plus 的自动上传，不需要配置 action 等参数
-             只需要做前端的本地预览图片即可，无需在提交前上传图标
-             语法：URL.createObjectURL(...) 创建本地预览的地址，来预览
-        -->
         <el-upload
           class="avatar-uploader"
           :show-file-list="false"
